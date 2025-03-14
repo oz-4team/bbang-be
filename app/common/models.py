b8a36b57a268aedb6bc4d2ef70307ffd8ca855a4
seonhongmun <mshg_@naver.com>
2025-03-14 11:15:53 +0900

    🐛Fix BaseModel

--- app/common/models.py
+++ app/common/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class BaseModel(models.Model):
     id = models.AutoField(primary_key=True)
-    created_at = models.DateTimeField('생성일',auto_now_add=True)
-    updated_at = models.DateTimeField('수정일',auto_now=True)
+    created_at = models.DateTimeField("생성일", auto_now_add=True)
+    updated_at = models.DateTimeField("수정일", auto_now=True)
 
     class Meta:
         abstract = True
