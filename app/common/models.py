488892660adef658da230d96b3b2ef279bcea45f
seonhongmun <mshg_@naver.com>
2025-03-14 11:15:30 +0900

    🐛Fix BaseModel

--- app/common/models.py
+++ app/common/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class BaseModel(models. Model):
+class BaseModel(models.Model):
     id = models.AutoField(primary_key=True)
     created_at = models.DateTimeField('생성일',auto_now_add=True)
     updated_at = models.DateTimeField('수정일',auto_now=True)
