b723c38d9eecd99bc0bbacfadcfd8da14aa8c2fd
seonhongmun <mshg_@naver.com>
2025-03-14 11:14:30 +0900

    ✨Add BaseModel

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models. Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField('생성일',auto_now_add=True)
+    updated_at = models.DateTimeField('수정일',auto_now=True)
+
+    class Meta:
+        abstract = True
