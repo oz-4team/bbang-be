ad15ea34ec2e19a376d7c829617fd51c6a6b6959
seonhongmun <mshg_@naver.com>
2025-03-14 17:51:22 +0900

    🎨account models.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "app.artists",
     "app.common",
     "app.schedule",
+    "app.content",
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "accounts.user"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
