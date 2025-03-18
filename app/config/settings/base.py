96b82415e617574181ad67ab22d340e321a96d16
seonhongmun <mshg_@naver.com>
2025-03-19 00:13:29 +0900

    ✨ add tokenblacklist

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django_extensions",
     "rest_framework",
     "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
     "drf_yasg",
     "app.accounts",
     "app.artists",
