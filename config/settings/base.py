47fcad64e7e59027a58f6df20ffd79e70eb50e62
seonhongmun <mshg_@naver.com>
2025-03-18 12:22:43 +0900

    🐛 bug fix

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,7 +97,7 @@ DATABASES = {
         "USER": os.environ.get("DATABASE_USER"),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
         "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_POAT"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
