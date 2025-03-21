b5df0e3a73c13527af3319ed51a5b9d7b4dcd6a5
Anianim <133176415+Anianim@users.noreply.github.com>
2025-03-21 21:15:33 +0900

    :sparkles: artist admin add

--- app/artists/admin.py
+++ app/artists/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ArtistGroup, Artist
 
-# Register your models here.
+class ArtistGroupAdmin(admin.ModelAdmin):
+    list_display = ('artist_group', 'artist_agency', 'group_insta')
+    search_fields = ('artist_group', 'artist_agency', 'group_insta')
+    list_filter = ('artist_agency',)
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ('artist_name', 'artist_group', 'artist_agency', 'artist_insta')
+    search_fields = ('artist_name', 'artist_group__artist_group', 'artist_agency', 'artist_insta')
+    list_filter = ('artist_agency', 'artist_group')
+    raw_id_fields = ('artist_group',)  # ForeignKey 필드에서 편리한 검색을 위해 raw_id_fields 사용
+
+admin.site.register(ArtistGroup, ArtistGroupAdmin)
+admin.site.register(Artist, ArtistAdmin)
--- app/artists/models.py
+++ app/artists/models.py
@@ -1,40 +1,11 @@
 from django.db import models
-
 from app.common.models import BaseModel
-
-
-# Create your models here.
-class ArtistImage(BaseModel):
-    image_name = models.CharField("이미지 이름", max_length=255, null=True, blank=True)
-    image_url = models.CharField("이미지 URL", max_length=255, null=True, blank=True)
-    image_type = models.CharField("이미지 타입", max_length=255, null=True, blank=True)
-
-    class Meta:
-        verbose_name = "아티스트 이미지"
-        verbose_name_plural = "아티스트 이미지 목록"
-
-    def __str__(self):
-        return self.image_name if self.image_name else "Unnamed Image"
-
-
-class ArtistGroupImage(BaseModel):
-    image_name = models.CharField("이미지 이름", max_length=255, null=True, blank=True)
-    image_url = models.CharField("이미지 URL", max_length=255, null=True, blank=True)
-    image_type = models.CharField("이미지 타입", max_length=255, null=True, blank=True)
-
-    class Meta:
-        verbose_name = "아티스트 그룹 이미지"
-        verbose_name_plural = "아티스트 그룹 이미지 목록"
-
-    def __str__(self):
-        return self.image_name if self.image_name else "Unnamed Image"
-
 
 class ArtistGroup(BaseModel):
     artist_group = models.CharField("아티스트 그룹", max_length=30, null=True, blank=True)
     artist_agency = models.CharField("소속사", max_length=30, null=True, blank=True)
     group_insta = models.CharField("인스타그램", max_length=30, null=True, blank=True)
-    image = models.ForeignKey(ArtistGroupImage, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField("그룹 이미지", upload_to="artist_groups/", null=True, blank=True)
 
     class Meta:
         verbose_name = "아티스트 그룹"
@@ -49,7 +20,7 @@ class Artist(BaseModel):
     artist_group = models.ForeignKey(ArtistGroup, on_delete=models.CASCADE, null=True, blank=True)
     artist_agency = models.CharField("소속사", max_length=30, null=True, blank=True)
     artist_insta = models.CharField("인스타그램", max_length=30, null=True, blank=True)
-    image = models.ForeignKey(ArtistImage, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField("아티스트 이미지", upload_to="artists/", null=True, blank=True)
 
     class Meta:
         verbose_name = "아티스트"
