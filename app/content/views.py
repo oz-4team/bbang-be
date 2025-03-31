a6eb5e6f2c30056d831a4ba6b01ddf99296cbd26
seonhongmun <mshg_@naver.com>
2025-03-31 10:18:17 +0900

    🎨 content 수정

--- app/content/views.py
+++ app/content/views.py
@@ -56,9 +56,9 @@ class AllLikesAPIView(APIView):
                 {
                     "like_id": like.id,  # 좋아요 고유 ID
                     "artist": like.artist.artist_name if like.artist else None,  # 아티스트 이름 (있으면)
-                    "artist_image": like.artist.image_url if like.artist and hasattr(like.artist, "image_url") else None,  # 아티스트 이미지 URL
                     "artist_group": like.artist_group.artist_group if like.artist_group else None,  # 아티스트 그룹 이름 (있으면)
-                    "artist_group_image": like.artist_group.image_url if like.artist_group and hasattr(like.artist_group, "image_url") else None,  # 아티스트 그룹 이미지 URL
+                    "artist_image": like.artist.image_url.url if like.artist and like.artist.image_url else None,
+                    "artist_group_image": like.artist_group.image_url.url if like.artist_group and like.artist_group.image_url else None,
                 }
                 for like in likes
             ]
