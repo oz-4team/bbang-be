8a1300650ed515319ac844692eb90fc6d120542d
seonhongmun <mshg_@naver.com>
2025-04-03 08:58:22 +0900

    🎨 content isort, black

--- app/content/views.py
+++ app/content/views.py
@@ -58,11 +58,11 @@ class AllLikesAPIView(APIView):
                 {
                     "like_id": like.id,  # 좋아요 고유 ID
                     "artist": like.artist.artist_name if like.artist else None,  # 아티스트 이름 (있으면)
-                    "artist_id" : like.artist.id if like.artist else None,  # 아티스트 ID (있으면)
+                    "artist_id": like.artist.id if like.artist else None,  # 아티스트 ID (있으면)
                     "artist_group": (
                         like.artist_group.artist_group if like.artist_group else None
                     ),  # 아티스트 그룹 이름 (있으면)
-                    "artist_group_id": like.artist_group.id if like.artist_group else None, #아티스트 그룹ID (있으면)
+                    "artist_group_id": like.artist_group.id if like.artist_group else None,  # 아티스트 그룹ID (있으면)
                     "artist_image": like.artist.image_url.url if like.artist and like.artist.image_url else None,
                     "artist_group_image": (
                         like.artist_group.image_url.url if like.artist_group and like.artist_group.image_url else None
@@ -77,6 +77,7 @@ class AllLikesAPIView(APIView):
                 {"message": "오류가 발생했습니다. 잠시 후 다시 시도해주세요."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
+
 
 # 특정 좋아요 조회 (단건 조회)
 class SingleLikeAPIView(APIView):
